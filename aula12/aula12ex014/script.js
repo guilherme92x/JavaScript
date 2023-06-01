e91a7e4214750427c0fd37a285bed6e5d6f42dda
guilherme92x <guilherme92x2@gmail.com>
2023-06-01 18:22:17 -0300

    Atualizei meu exercicios
    Estou rafazendo o curso

--- aula12/aula12ex014/script.js
+++ aula12/aula12ex014/script.js
@@ -8,11 +8,14 @@ function carregar(){
     if (hora >= 0 && hora < 18) {
         //BOM DIA!
         img.src = `imagens/fotomanha.png`
+        document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora < 18) {
         //BOA TARDE!
         img.src = `imagens/fototarde.png`
+        document.body.style.background = '#b9846f'
     } else {
         //BOA NOITE!
         img.src = `imagens/fotonoite.png`
+        document.body.style.background = '#515154'
     }
-}
+} 
